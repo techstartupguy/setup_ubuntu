fdfe672dac1b9f6c984266a4bd60181c10f581d0
Gaurav Gupta <gg.spark@gmail.com>
2014-05-30 04:30:44 +0800

    updated catkin configuration

--- ros.sh
+++ ros.sh
@@ -17,10 +17,11 @@ rosdep update
 sudo apt-get install -y ros-hydro-joy
 sudo apt-get install -y python-rosinstall
 
-echo "sudo sed -i 's/CATKIN_SHELL=$CATKIN_SHELL "$_SETUP_UTIL" $@ > $_SETUP_TMP/CATKIN_SHELL=$CATKIN_SHELL "$_SETUP_UTIL" $@ >> $_SETUP_TMP/g' /opt/ros/hydro/setup.sh" >> .bashrc
-echo "sudo sed -i 's/while [ $_i -lt $_CATKIN_ENVIRONMENT_HOOKS_COUNT ]; do/while [[ $_i -lt $_CATKIN_ENVIRONMENT_HOOKS_COUNT ]]; do/g' /opt/ros/hydro/setup.sh" >> .bashrc 
 echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
+echo "sed -i 's/while [  -lt  ]; do/while [[  -lt  ]]; do/g' ~/catkin_ws/devel/setup.sh" >> .bashrc
+echo "sed -i 's/CATKIN_SHELL=bash   > /CATKIN_SHELL=bash   >> /g' ~/catkin_ws/devel/setup.sh" >> .bashrc 
 echo "source ~/catkin_ws/install/setup.bash" >> .bashrc
+echo "#source ~/catkin_ws/devel/setup.sh" >> .bashrc
 
 
 sudo sed -i 's/CATKIN_SHELL=$CATKIN_SHELL "$_SETUP_UTIL" $@ > $_SETUP_TMP/CATKIN_SHELL=$CATKIN_SHELL "$_SETUP_UTIL" $@ >> $_SETUP_TMP/g' /opt/ros/hydro/setup.sh
